ee53cb762d8dd8b0d0ef5e2c30f161de162e9e19
Luke Buscombe <luke.buscombe@gmail.com>
2011-11-19 00:29:30 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lacuna::Application.config.secret_token = 'bdabed3f776c5c06babfac84a99703192a99798138914542153d59dc96823ca968bc5e2cc1ec10dbf9112f945d2e71bc5441ca255a2bbfde4033d82fe10590af'
